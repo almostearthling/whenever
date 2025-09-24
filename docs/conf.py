11bdb9021b19e9fb1b976555d1d76cafc2ecc959
Francesco Garosi <francesco.garosi@gmail.com>
2025-09-24 18:01:54 +0200

    docs: remove reference to JSON for DBus params

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "The Whenever Scheduler"
 author = "Francesco Garosi"
 copyright = "2023-%Y, Francesco Garosi"
-release = "0.4.10"
+release = "0.4.11"
 
 html_logo = "graphics/metronome.png"
 
