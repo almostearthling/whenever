50c12acbd40673ba6c18c3f917ff86dc9d46ca24
Francesco Garosi <francesco.garosi@gmail.com>
2026-02-12 00:09:58 +0100

    feat: attach exe icon and properties on Windows
    This version, on Windows, produces an executable that contains the metronome icon, version information and other properties such as the product name and the description; no other feature is added, and the Linux version remains unaffected: however, being a feature, this bumps the version number to 1.1 as per guidelines

--- build.rs
+++ build.rs
@@ -8,27 +8,26 @@ const APP_DESC: &str = env!("CARGO_PKG_DESCRIPTION");
 const APP_VERSION: &str = env!("CARGO_PKG_VERSION");
 const APP_LICENSE: &str = env!("CARGO_PKG_LICENSE");
 
+const APP_VER_MAJOR: &str = env!("CARGO_PKG_VERSION_MAJOR");
+const APP_VER_MINOR: &str = env!("CARGO_PKG_VERSION_MINOR");
+const APP_VER_PATCH: &str = env!("CARGO_PKG_VERSION_PATCH");
+
 
 // provide version info as a 64 bit unsigned: use the provided version string
 // to (somewhat arbitrarily) extract the information, although it should be
 // consistent when following the versioning guideline: see
 // https://github.com/almostearthling/whenever/discussions/88#discussion-9422899
-fn version_info_64bit() -> u64 {
-    let mut ver: Vec<u64> = Vec::new();
-    for s in APP_VERSION.split(".") {
-        ver.push(if s.contains("-") {
-            u64::from_str_radix(s.split("-").next().unwrap_or("0"), 10).unwrap_or(0)
-        } else {
-            u64::from_str_radix(s, 10).unwrap_or(0)
-        });
-    };
+fn version_info_as_u64() -> u64 {
+    let app_ver_pre: &str = option_env!("CARGO_PKG_VERSION_PRE").unwrap_or("0");
 
-    ver[0] << 48 + ver[1] << 32 + ver[2] << 16 + if ver.len() > 3 { ver[3] } else { 0 }
+    u64::from_str_radix(APP_VER_MAJOR, 10).unwrap_or(0) << 48
+    | u64::from_str_radix(APP_VER_MINOR, 10).unwrap_or(0) << 32
+    | u64::from_str_radix(APP_VER_PATCH, 10).unwrap_or(0) << 16
+    | u64::from_str_radix(app_ver_pre, 10).unwrap_or(0) << 0
 }
 
 
 fn main() {
-
     // attach an icon and version information when the target OS is Windows
     if let Ok(platform) = env::var("CARGO_CFG_TARGET_OS") {
         if platform == "windows" {
@@ -37,7 +36,10 @@ fn main() {
             let mut res = winresource::WindowsResource::new();
             res
                 .set_icon("resources/metronome.ico")
-                .set_version_info(winresource::VersionInfo::PRODUCTVERSION, version_info_64bit())
+                .set_version_info(
+                    winresource::VersionInfo::PRODUCTVERSION, 
+                    version_info_as_u64(),
+                )
                 .set("InternalName", APP_NAME)
                 .set("FileDescription", APP_DESC)
                 .set("ProductVersion", APP_VERSION)
