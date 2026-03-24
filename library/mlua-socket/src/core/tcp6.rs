0ef17e5336df1b44a02606034b9c663e714b261a
Francesco Garosi <francesco.garosi@gmail.com>
2026-03-24 19:23:42 +0100

    temp: remove `mlua-socket` from the source tree

--- library/mlua-socket/src/core/tcp6.rs
+++
@@ -1,8 +0,0 @@
-use super::tcp::Tcp;
-use mlua::{Error, Lua, MultiValue};
-use socket2::{Domain, Socket, Type};
-
-pub(super) fn handle(_lua: &Lua, _args: MultiValue) -> Result<Tcp, Error> {
-    let socket = Socket::new(Domain::IPV6, Type::STREAM, None)?;
-    Ok(Tcp { socket })
-}
